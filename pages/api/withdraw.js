1fff6ebef256914f4c041f3d469c39e43112eb97
denverpk <149782686+denverpk@users.noreply.github.com>
2025-08-06 01:28:19 +0530

    Update withdraw.js

--- pages/api/withdraw.js
+++ pages/api/withdraw.js
@@ -4,10 +4,10 @@ import { BiconomyPaymaster } from '@biconomy/paymaster';
 export default async function handler(req, res) {
   try {
     const provider = new ethers.providers.JsonRpcProvider('https://mainnet.base.org'); // Use your RPC
-    const signer = new ethers.Wallet('YOUR_PRIVATE_KEY', provider); // Replace with Paymaster Admin Wallet
-    const paymaster = new BiconomyPaymaster({ paymasterApiKey: 'YOUR_PAYMASTER_API_KEY' });
+    const signer = new ethers.Wallet('0x8a95fefd533ab0d2e5249e0a16b08df3dfc5a8be0996cc72a7ef406f5d9ae3a7', provider); // Replace with Paymaster Admin Wallet
+    const paymaster = new BiconomyPaymaster({ paymasterApiKey: 'TbS1iirlF.a1bad543-0e96-44d9-bc55-6d9e833f88a4' });
 
-    const withdrawTx = await paymaster.withdrawFunds(signer, 'DESTINATION_WALLET_ADDRESS');
+    const withdrawTx = await paymaster.withdrawFunds(signer, '0x73Bd6A6150Ff2527358d21e6bb5E93f287E01f96');
     await withdrawTx.wait();
 
     res.status(200).json({ message: 'Funds Withdrawn Successfully!' });
